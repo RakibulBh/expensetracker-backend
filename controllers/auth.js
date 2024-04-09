29ca61bc4ba83e5a5358cd646a72e63e1a1cd244
RakibulB <rakibulbhuiyan1973@gmail.com>
2024-04-09 04:46:47 +0100

    added user validation for signup and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,27 @@
 const jwt = require('jsonwebtoken');
 const express = require('express');
 const bcrypt = require('bcrypt');
+const validator = require('validator'); // Import the validator package
 const User = require('../models/User');
 
 const createToken = (_id) => {
-
     return jwt.sign({_id }, process.env.JWT_SECRET, { expiresIn: '3d' });
-
 }
 
 /* register */
 
 const registerUser = async (req, res) => {
     const { firstName, lastName, email, password } = req.body;
+
+    // Validate email
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ message: 'Invalid email format' });
+    }
+
+    // Validate password
+    if (!validator.isStrongPassword(password)) {
+        return res.status(400).json({ message: 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one symbol' });
+    }
 
     try {
         const user = await User.register(firstName, lastName, email, password);
@@ -32,6 +41,11 @@ const registerUser = async (req, res) => {
 const logUser = async (req, res) => {
     const { email, password } = req.body;
 
+    // Validate email
+    if (!validator.isEmail(email)) {
+        return res.status(400).json({ message: 'Invalid email format' });
+    }
+
     try {
         const user = await User.login(email, password);
         const token = createToken(user._id);
@@ -42,9 +56,7 @@ const logUser = async (req, res) => {
     }
 };
 
-
 module.exports = {
     registerUser,
     logUser
 }
-
