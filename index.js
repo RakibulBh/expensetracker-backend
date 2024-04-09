a42d511a14fa1f25935ab7c7614c3f422da46a0b
RakibulB <rakibulbhuiyan1973@gmail.com>
2024-04-09 04:34:08 +0100

    config

--- index.js
+++ index.js
@@ -21,10 +21,7 @@ app.use(helmet());
 app.use(morgan("common"));
 app.use(bodyParser.json({limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
-app.use(cors({
-origin: ['https://expensetracker-frontend-eosin.vercel.app'],
-        credentials: true
-}));
+app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
 
 
